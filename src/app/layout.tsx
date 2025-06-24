b8a1497379ab3b9d91e85a7d0f5aa03444f9c8af
Bry Low <bmanmarlow@gmail.com>
2025-06-24 16:49:08 +0000

    It still hasn't changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&family=Lexend:wght@100..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased h-screen w-screen overflow-hidden flex flex-col">
+      <body className="font-body antialiased h-screen w-screen flex flex-col">
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
